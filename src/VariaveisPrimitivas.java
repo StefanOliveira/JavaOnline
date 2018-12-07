9146efd263fff12f29313e4eabc58c57a3ca77f7
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-12-07 13:53:12 -0200

    Variaveis_Primitivas

---
+++ src/VariaveisPrimitivas.java
@@ -0,0 +1,15 @@
+public class VariaveisPrimitivas {
+    public static void main(String[] args){
+        int idade=10;
+        double salarioDouble = 3000;
+        float salariofloat = 3000;
+        byte idadeByte = 12;
+        short idadeShort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        long numeroGrande = 1000L;
+        char caractere = 'A'; //2 bytes, pode-se usar valores hexadecimais \u0041 ou números de 2 bytes
+        String nome = "William";
+        System.out.println(caractere);
+    }
+}
