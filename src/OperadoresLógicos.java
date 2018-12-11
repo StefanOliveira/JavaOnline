1baeda3f4f0efa15f12e9f8881387824b1ea4545
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-12-11 16:24:31 -0200

    Operadores Lógicos

---
+++ src/OperadoresLógicos.java
@@ -0,0 +1,10 @@
+public class OperadoresLógicos {
+    public static void main(String[] args){
+       int idade = 18;
+       float salario = 1000f;
+
+       System.out.println(idade >= 20 || salario >= 2000); //Operador OR
+       System.out.println(idade >= 20 && salario >= 2000); //Operador AND
+    }
+}
+
