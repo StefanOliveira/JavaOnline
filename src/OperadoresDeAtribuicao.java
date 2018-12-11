fe25296e4a033f7e7bf9df3649eefba6c1227076
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-12-11 17:01:15 -0200

    Operadores Lógicos

---
+++ src/OperadoresDeAtribuicao.java
@@ -0,0 +1,18 @@
+public class OperadoresDeAtribuicao {
+    public static void main(String[] args) {
+        int salario = 1800;
+        int numero = 11;
+
+        salario += 1000;
+        System.out.println(salario);
+
+        salario -= 1000;
+        System.out.println(salario);
+
+        salario /= 1000;
+        System.out.println(salario);
+
+        salario %= numero;
+        System.out.println(salario);
+    }
+}
