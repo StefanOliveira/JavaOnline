3eef18d1c0162d1f16c5e649e6cb9a2b322885a2
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-12-11 15:18:42 -0200

    Variaveis_Primitivas

---
+++ src/Operadores.java
@@ -0,0 +1,14 @@
+/**
+ * Operadores Básicos
+ */
+
+public class Operadores {
+    public static void main(String[] args){
+        int numero1 = 10;
+        int numero2 = 20;
+        int soma = numero1 + numero2;
+        // 2 formas de se imprimir os número acima
+        System.out.println("O resultado da soma é: " + soma); //Resultado concatenado
+        System.out.println("O resultado da soma é: " + (numero1+numero2)); //Resultado com operação matematica
+    }
+}
