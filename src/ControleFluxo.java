41aca473c7ecf4b09735081dba2f876db26c733d
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-12-11 17:58:38 -0200

    Controle de Fluxo

---
+++ src/ControleFluxo.java
@@ -0,0 +1,18 @@
+public class ControleFluxo {
+    public static void main(String[] args) {
+        // idade < 15 Categoria Infantil
+        // idade >= 15 && idade < 18 Juvenil
+        // idade >= 18 Adulto
+        int idade = 18;
+        String categoria;
+
+        if(idade < 12){
+            categoria = "Categoria Infantil";
+        }else if(idade >= 12 && idade < 18){
+            categoria = "Categoria Juvenil";
+        }else{
+            categoria = "Categoria Adulta";
+        }
+        System.out.println(categoria);
+    }
+}
