b15cf2f7d70936c7ceb58ab6a9ccb73711a8abd0
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-12-11 15:52:24 -0200

    Operadores

--- src/Operadores.java
+++ src/Operadores.java
@@ -7,8 +7,14 @@ public class Operadores {
         int numero1 = 10;
         int numero2 = 20;
         int soma = numero1 + numero2;
+        boolean dezMaiorQueVinte = 10 >= 20;
+        boolean dezMenorIgualVinte = 10 <= 20;
         // 2 formas de se imprimir os número acima
         System.out.println("O resultado da soma é: " + soma); //Resultado concatenado
         System.out.println("O resultado da soma é: " + (numero1+numero2)); //Resultado com operação matematica
+        System.out.println(dezMaiorQueVinte);
+        System.out.println(dezMenorIgualVinte);
+        System.out.println(5==5);
+        System.out.println(5 != 5);
     }
 }
