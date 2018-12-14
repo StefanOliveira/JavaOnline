61a526f3c288f26bc8ce253fb22a34cee3baf396
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-12-14 16:49:15 -0200

    Controle de Fluxo

---
+++ src/Arrays2.java
@@ -0,0 +1,24 @@
+/**
+ * aula 19 Arrays parte 2
+ */
+
+
+public class Arrays2 {
+    public static void main(String[] args) {
+        /*
+        byte, short, int, long, float, double = 0
+        char = '' '\u0000'
+        boolean = false
+        String/reference = null
+        */
+        String[] nomes = new String[3];
+        nomes[0] = "Sophia";
+        nomes[1] = "Stefan";
+        nomes[2] = "Érika";
+
+        for(int i=0; i < nomes.length; i++){
+            System.out.println((i + 1) + " Nome " + nomes[i]);
+        }
+
+    }
+}
--- src/ControleFluxo6.java
+++ src/ControleFluxo6.java
@@ -1,22 +1,22 @@
-import javax.xml.bind.SchemaOutputResolver;
+import org.omg.CORBA.portable.ValueOutputStream;
 
 /**
  * Dado o valor de um carro, descubra quantas vezes ele pode ser parcelado
  * Porém as parcelas precisam ser maiores do que 1000
  */
 
-public class ControleFluxo5 {
+
+public class ControleFluxo6 {
     public static void main(String[] args) {
 
         double valorTotal = 30000;
 
-        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+        for(int parcela = (int) valorTotal; parcela >= 1; parcela--) {
             double valorParcela = valorTotal / parcela;
-            if (valorParcela < 1000) {
-                break;
+            if (valorParcela <= 1000) {
+               continue;
             }
             System.out.println("Parcelamento em " + parcela + " vezes, valor da Parcela é: R$" + valorParcela);
-
         }
     }
 }
