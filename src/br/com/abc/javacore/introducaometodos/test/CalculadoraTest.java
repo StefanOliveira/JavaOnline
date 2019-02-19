0191f49e2ace43149e8022127c5f619403a1ca67
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2019-02-19 14:21:48 -0300

    classes

---
+++ src/br/com/abc/javacore/introducaometodos/test/CalculadoraTest.java
@@ -0,0 +1,13 @@
+package br.com.abc.javacore.introducaometodos.test;
+
+import br.com.abc.javacore.introducaometodos.classes.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        calc.somaDoisNumeros();
+        calc.subtraiDoisNumeros();
+        System.out.println("Continuando a exec");
+
+    }
+}
