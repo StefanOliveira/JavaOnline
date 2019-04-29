09665155dcc27c3942667d51be9761d4828176dd
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2019-04-29 12:12:39 -0300

    classes

---
+++ src/cursoEstacio/SomaCode.java
@@ -0,0 +1,13 @@
+package cursoEstacio;
+
+public class SomaCode {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 8;
+        int soma;
+
+        soma = num1 + num2;
+
+        System.out.printf("A soma é %d\n", soma);
+    }
+}
